aa50a8c89df1860df4b7e2ac3e6f9d34e0d805a8
Atanas Krondev <krondev@gmail.com>
2020-02-12 16:12:46 +0200

    initial setup

---
+++ ten-meme/src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { CardComponent } from './card/card.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    NavigationComponent,
+    CardComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ten-meme/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD_j1ueJ8lsjip68UlDDBNL14peEpCB5qI",
+    authDomain: "ten-meme.firebaseapp.com",
+    databaseURL: "https://ten-meme.firebaseio.com",
+    projectId: "ten-meme",
+    storageBucket: "ten-meme.appspot.com",
+    messagingSenderId: "575361632609",
+    appId: "1:575361632609:web:433aafa81dc90fe0f5dd20",
+    measurementId: "G-FZ46RTXK0H"
+  }
+};
